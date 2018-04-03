d109aaa6e35f11b7f58c7c85997246c4a196814a
Wesley Su <wesley.su@richmond.edu>
2018-04-03 19:08:07 -0400

    Continued working on tester

--- tester.cpp
+++ tester.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 
-	cout << "Testing getters/setters:" endl;
+	cout << "Testing getters/setters:" << endl;
 	cout << "--------------------------------------------" << endl << endl;
 
 	cout << "Person p;" << endl;
@@ -14,22 +14,37 @@ int main()
 
 	cout << "p.setFirstName(\"LeBron\")" << endl;
 	p.setFirstName("LeBron");
-	cout << "First Name: " < p.getFirstName() << " [Expected: LeBron]" << endl;
+	cout << "First Name: " << p.getFirstName() << " [Expected: LeBron]" << endl << endl;
 
 	cout << "p.setLastName(\"James\")" << endl;
 	p.setLastName("James");
-	cout << "Last Name: " < p.getLastName() << " [Expected: James]" << endl;
+	cout << "Last Name: " << p.getLastName() << " [Expected: James]" << endl << endl;
 
-
+	cout << "p.setDateOfBirth(30, 12, 1984)" << endl;
 	p.setDateOfBirth(30, 12, 1984);
-	cout << "DateOfBirth: " < p.getFirstName() << " [Expected: LeBron]" << endl;
+	struct tm year = p.getDateOfBirth();
+	cout << "Date of birth: " << year.tm_mon << "/" << year.tm_mday << "/"
+		 << year.tm_year << " [Expected: 12/30/1984]" << endl << endl;
 
-
+	cout << "p.setAddress(\"Cleveland and Los Angeles\")" << endl;
 	p.setAddress("Cleveland and Los Angeles");
+	cout << "Address: " << p.getAddress() << " [Expected: Cleveland and Los Angeles]"  << endl << endl;
+
+	cout << "p.setNetID(\"GOAT\")" << endl;
 	p.setNetID("GOAT");
+	cout << "NetID: " << p.getNetID() << " [Expected: GOAT]" << endl << endl;
+
+	cout << "p.setURID(23)" << endl;
 	p.setURID(23);
+	cout << "URID: " << p.getNetID() << " [Expected: 23]" << endl << endl;
+
+	cout << "p.setEmail(\"goat@goat.com\")" << endl;
 	p.setEmail("goat@goat.com");
+	cout << "Email: " << p.getEmail() << " [Expected: goat@goat.com]" << endl << endl;
+
+	cout << "p.setPhone(2162134628)" << endl;
 	p.setPhone(2162134628);
+	cout << "Phone: " << p.getPhone() << " [Expected: 2162134628]" << endl << endl;
 
 
 
