082247bf7e3dd76d55b5b0ebf5cb874fe8502b20
mx-xn <maxine.xin@richmond.edu>
2018-04-11 14:16:44 -0400

    test

--- lab11/tester.cpp
+++ lab11/tester.cpp
@@ -4,6 +4,26 @@
 using namespace std;
 
 int main() {
+	cout << "Test LinkedList:-----------------" << endl << endl;
         LinkedList<int> l;
+	cout << "Test add and size: +++++++++++++++++++" << endl << endl;
+	cout << "before add:" << endl;
+	cout << "size:" << endl;
+	cout << l.size() << " -- [0]" << endl;
+	
+	cout << "after add:" << endl;
+	l.add(1);
+	l.add(2);
+	cout << l.get(0) << " -- [1]" << endl;
+	cout << l.get(1) << " -- [2]" << endl;
+	cout << "size:" << endl;
+	cout << l.size() << " -- [2]" << end;
+
+	
+	
+
+		
+
+
         return 0;
 }
